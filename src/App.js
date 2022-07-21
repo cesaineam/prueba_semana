2c1b323a816c662d021f3150eec954df4f4f6d8c
cesaineam <cesaineam@unal.edu.co>
2022-07-20 20:40:33 -0500

    Configuracion de bootrasp y estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import './Styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1> Mi aplicacion </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    
+    
+  
+    <header class="jumbotron">
+        <div class="container">
+            <div class="row row-header">
+                <div class="col-12 col-sm-6">
+                    <h1 class="font-weight">Blog de presentacion</h1>
+                    
+                </div>
+                <div class="col-12 col-sm-3 align-self-center">
+
+                   
+
+                </div>
+                <div class="col-12 col-sm align-self-center">
+                    <a role="button" className= "btn btn-info" href="https://www.linkedin.com/in/cristian-esteban-sainea-martinez-7011561b7/">Linkedin Cristian Sainea </a>
+                </div>
+            </div>
+        </div>
+    </header>
+  </div>
+    
   );
 }
 
